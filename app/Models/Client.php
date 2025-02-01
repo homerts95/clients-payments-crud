08933bc90fcbf4a1cd56f27a2163cdf7b2f0a6c5
omiros tsintilas <h.tsintilas@gmail.com>
2025-02-01 13:10:05 +0200

    client and payment models

---
+++ app/Models/Client.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    public const TABLE_NAME = 'clients';
+    public const NAME = 'name';
+    public const SURNAME = 'surname';
+    public const CREATED_AT = 'created_at';
+
+    public const UPDATED_AT = 'updated_at';
+
+    protected $fillable = [
+        self::NAME,
+        self::SURNAME,
+    ];
+    protected $casts = [
+        self::CREATED_AT => 'datetime',
+        self::UPDATED_AT => 'datetime',
+    ];
+}
---
+++ app/Models/Payment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    const TABLE_NAME = 'payments';
+    const AMOUNT = 'amount';
+    const CLIENT_ID = 'client_id';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $fillable = [
+        self::AMOUNT,
+        self::CLIENT_ID,
+    ];
+
+    protected $casts = [
+        self::CREATED_AT => 'datetime',
+        self::UPDATED_AT => 'datetime',
+        self::AMOUNT => 'integer',
+    ];
+}
