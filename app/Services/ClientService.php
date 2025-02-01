15a7c6a4e500020916b36095acf3b20be8e933a2
omiros tsintilas <h.tsintilas@gmail.com>
2025-02-01 17:02:27 +0200

    spacing fix

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -11,27 +11,28 @@ class ClientService
     )
     {
     }
-        public function create(array $data)
+
+    public function create(array $data)
     {
         return $this->clientRepository->create($data);
     }
 
-        public function update(array $data, $id)
+    public function update(array $data, $id)
     {
         return $this->clientRepository->update($data, $id);
     }
 
-        public function delete($id)
+    public function delete($id)
     {
         return $this->clientRepository->delete($id);
     }
 
-        public function all()
+    public function all()
     {
         return $this->clientRepository->all();
     }
 
-        public function find($id)
+    public function find($id)
     {
         return $this->clientRepository->find($id);
     }
