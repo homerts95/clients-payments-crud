b28d6970a0bb45c4f5fad540b206a1a73cdef759
omiros tsintilas <h.tsintilas@gmail.com>
2025-02-02 21:56:53 +0200

    modified seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -17,26 +17,32 @@ class ClientSeeder extends Seeder
             [
                 'name' => 'Taylor',
                 'surname' => 'Otwell',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' =>'2025-01-01 17:25:52',
+                'updated_at' =>'2025-01-01 17:25:52',
             ],
             [
                 'name' => 'Taylor',
                 'surname' => 'Taylor',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => '2025-01-02 17:25:52',
+                'updated_at' => '2025-01-02 17:25:52',
             ],
             [
                 'name' => 'Jeffrey',
                 'surname' => 'Way',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' =>'2025-02-01 17:25:52',
+                'updated_at' =>'2025-02-01 17:25:52',
+            ],
+            [
+                'name' => 'Jeffrey',
+                'surname' => 'Way',
+                'created_at' =>'2025-02-02 17:25:52',
+                'updated_at' =>'2025-02-02 17:25:52',
             ],
             [
                 'name' => 'Phill',
                 'surname' => 'Sparks',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' =>'2025-03-01 17:25:52',
+                'updated_at' =>'2025-03-02 17:25:52',
             ],
         ];
 
